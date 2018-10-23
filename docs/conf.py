7c98eff8a4c17d556546dbbd6344f2167f0279b6
SpinoWeb <spinoweb@hotmail.com>
2018-10-23 18:25:40 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -55,8 +55,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'Calcolazioni'
+copyright = u'2018, calcolazioni'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -201,7 +201,7 @@ html_use_smartypants = True
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'Calcolazionidoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -221,8 +221,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'CalcolazioniTemplate.tex', u'Calcolazioni Template Documentation',
+   u'Calcolazioni', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -251,8 +251,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'calcolazionitemplate', u'Calcolazioni Template Documentation',
+     [u'Calcolazioni'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -265,8 +265,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'CalcolazioniTemplate', u'Calcolazioni Template Documentation',
+   u'Calcolazioni', 'CalcolazioniTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -285,10 +285,10 @@ texinfo_documents = [
 # -- Options for Epub output ---------------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = u'Wiser'
-epub_author = u'Fernanda Nery'
-epub_publisher = u'Fernanda Nery'
-epub_copyright = u'2014, Fernanda Nery'
+epub_title = u'Calcolazioni'
+epub_author = u'N S'
+epub_publisher = u'N S'
+epub_copyright = u'2018, N S'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
